de8e4763def0ba40e516d4831037851b5341e2ef
Xuan Wang <156254316+Borie030125@users.noreply.github.com>
2024-04-25 18:19:59 -0400

    Update/Test vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gamehub-Bories030125/',
+  base: '/gamehub-team10/',
 })
